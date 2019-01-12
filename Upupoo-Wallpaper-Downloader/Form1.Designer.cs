fed713e81e67498ed8ffa80537cb7951007fcefd
凌莞 <i@gao4.pw>
2019-01-12 21:43:05 +0800

    添加项目文件。

---
+++ Upupoo-Wallpaper-Downloader/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Upupoo_Wallpaper_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "适用于 Upupoo 壁纸商店的视频壁纸\r\n可访问 http://wallpaper.upupoo.com/store/browVi/1-0-0-1.htm 获取" +
+    "链接\r\n例如 http://wallpaper.upupoo.com/store/paperDetail-1800068298.htm\r\n在下面的编辑框中输入预" +
+    "览页面的网址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始解析直链";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(410, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "下面的编辑框显示解析出的直链地址\r\n你可以下载后放入 Wallpaper Engine 之类的软件使用";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "浏览器下载";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 260);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Upupoo 壁纸直链提取器 1.0 by 凌莞";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Upupoo-Wallpaper-Downloader/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upupoo_Wallpaper_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string link = textBox1.Text;
+            // http://wallpaper.upupoo.com/store/paperDetail-1800068298.htm
+            link = link.Between("paperDetail-", ".htm");
+            string url = $"http://source.upupoo.com/theme/{link}/theme.upup";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream datastream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(datastream, Encoding.UTF8);
+            string r = reader.ReadToEnd();
+            reader.Close();
+            datastream.Close();
+            response.Close();
+            // {"ver":"'1'","src":"0001.哔哩哔哩-【自制展示】pop子和pipi美的日常动态桌面展示-木马镜像[超清版].mp4","author":"1001157437","themeno":1800068298,"name":"pop子和pipi美的日常动态桌","themeType":1,"tag":"12","url":"http://source.upupoo.com/theme/1800068298/theme.upup"}
+            r = r.Between("\"src\":\"", "\"");
+            textBox2.Text = $"http://source.upupoo.com/theme/{link}/{r}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(textBox2.Text);
+        }
+    }
+
+
+    public static class StringHelper
+    {
+        /// <summary>
+        /// 取文本左边内容
+        /// </summary>
+        /// <param name="str">文本</param>
+        /// <param name="s">标识符</param>
+        /// <returns>左边内容</returns>
+        public static string GetLeft(this string str, string s)
+        {
+            string temp = str.Substring(0, str.IndexOf(s));
+            return temp;
+        }
+
+
+        /// <summary>
+        /// 取文本右边内容
+        /// </summary>
+        /// <param name="str">文本</param>
+        /// <param name="s">标识符</param>
+        /// <returns>右边内容</returns>
+        public static string GetRight(this string str, string s)
+        {
+            string temp = str.Substring(str.IndexOf(s) + s.Length);
+            return temp;
+        }
+
+        /// <summary>
+        /// 取文本中间内容
+        /// </summary>
+        /// <param name="str">原文本</param>
+        /// <param name="leftstr">左边文本</param>
+        /// <param name="rightstr">右边文本</param>
+        /// <returns>返回中间文本内容</returns>
+        public static string Between(this string str, string leftstr, string rightstr)
+        {
+            int i = str.IndexOf(leftstr) + leftstr.Length;
+            string temp = str.Substring(i, str.IndexOf(rightstr, i) - i);
+            return temp;
+        }
+    }
+}
